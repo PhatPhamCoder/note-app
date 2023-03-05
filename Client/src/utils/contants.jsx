3ff36a008ae4984cd7ee01c69552992956373798
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-03-05 16:13:19 +0700

    update

--- Client/src/utils/contants.jsx
+++ Client/src/utils/contants.jsx
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = "http://localhost:4000";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000/graphql";
+export const GRAPHQL_SERVER = "https://note-app-8w97.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://note-app-8w97.onrender.com";
